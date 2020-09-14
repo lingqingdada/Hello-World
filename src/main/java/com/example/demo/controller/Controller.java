36acb2740c9a3d2ae4f1f1400af1324f0570a77c
lingqing <1974875650@qq.com>
2020-09-14 16:03:00 +0800

    增加了controller包和一个Controller类

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,9 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@org.springframework.stereotype.Controller
+
+@RestController
+public class Controller {
+}
